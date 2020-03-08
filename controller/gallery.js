0912061e0db979de53b2d5d3b12012c37157c94f
Kenneth <kennethvdstrieckt@hotmail.com>
2020-03-08 15:49:04 +0100

    gallery constructions

--- controller/gallery.js
+++ controller/gallery.js
@@ -2,31 +2,27 @@ jQuery(document).ready(function($){
 
 	/************** Menu Content Opening *********************/
 	$(".main_menu a, .responsive_menu a").click(function(){
-		var id =  $(this).attr('class');
-		id = id.split('-');
+		$('.overlay a').remove();
 		$("#menu-container .content").hide();
-		$("#menu-container #menu-"+id[1]).addClass("animated fadeInDown").show();
 		$("#menu-container .homepage").hide();
+		var eventSelection = $(this).attr('at'); 
+		var url = '../../assets/images/gallery/' + eventSelection + '/';
+		var smallUrl = '../../assets/images/gallery/' + eventSelection + '/square/';
+		for (var image = 1; image <= 36; image++) {
+			galleryPicture = url + image + '.jpg';
+			smallPicture = smallUrl + image + '.jpg';
+			
+			console.log(smallPicture);
+			console.log(galleryPicture);
+		};
+		$('.overlay').append(`<a href="${galleryPicture}"  data-rel="lightbox" class="fa fa-expand"></a>`);
+		$('.hexagon-in2').css(`background-image: url("${smallPicture}")`);
 		return false;
 	});
 
-	function imagelink() {
-		var eventSelection = $('gallery').attr('at');
-		var url = '../../assets/images/gallery/' + eventSelection + '/';
-		var smallUrl = '../../assets/images/gallery/' + eventSelection + '/square/';
-		for (var image = 0; image <= 36; image++) {
-			let galleryPicture = url + image + '.jpg';
-			let smallPicture = smallUrl + image + '.jpg';
-			$('.overlay').append(`<a href='${galleryPicture} '  data-rel="lightbox" class="fa fa-expand"></a>`);
-			$('.hexagon-in2').css(`background-image: url(${smallPicture});`);
-			console.log(smallPicture);
-			console.log(galleryPicture);
-		}
-	}; 
 
 	$(".main_menu a.home, .responsive_menu a.home").click(function(){
 		$("#menu-container .homepage").addClass("animated fadeInDown").show();
-		imagelink();
 		return false;
 	});
 
@@ -66,12 +62,4 @@ function loadScript() {
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
       'callback=initialize';
   document.body.appendChild(script);
-}
-
-function initialize() {
-    var mapOptions = {
-      zoom: 12,
-      center: new google.maps.LatLng(40.7823234,-73.9654161)
-    };
-    var map = new google.maps.Map(document.getElementById('map'),  mapOptions);
 }
